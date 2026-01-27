f184ac72193981586fdabff2cebd46b3a491ab6c
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-27 10:23:30 -0500

    feat: add site to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tusitio.com',
+  site: 'https://mazmorrdev.github.io/portfolio.github.io/',
+  base: '/portfolio.github.io/',
   integrations: []
 });
