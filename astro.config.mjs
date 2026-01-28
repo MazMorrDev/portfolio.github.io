8a14e9d9648b067c8885e9441200cc8fe57abb1a
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-28 05:39:19 -0500

    ref(astro.config.mjs): remove base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // ⚠️ SOLO el dominio base, SIN la subruta
   site: 'https://mazmorrdev.github.io',
-  
-  // ⚠️ SOLO la subruta, sin el dominio
-  base: '/portfolio.github.io/',
-  
   integrations: []
 });
