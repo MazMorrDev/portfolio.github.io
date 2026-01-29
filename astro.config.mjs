20377589afd43ea1e18e1fde52108d4205bd7c8e
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-29 07:40:26 -0500

    ref: trying to solve path problems in build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // ⚠️ SOLO el dominio base, SIN la subruta
-  site: 'https://mazmorrdev.github.io',
-  base: '/portfolio/', 
+  site: 'https://mazmorrdev.is-a.dev',
+  build: {
+    format: 'directory',
+  },
+  trailingSlash: 'always',
   integrations: []
 });
